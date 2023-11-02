1b6d5eae2b7b5f3388a29866b34f783629211ecb
alyconr <alyconr@hotmail.com>
2023-11-02 16:28:21 -0500

    :construction: Adjust router

--- Backend/app.js
+++ Backend/app.js
@@ -5,17 +5,18 @@ const app = express();
 
 const cors = require("cors");
 
-const dbUrl = process.env.MYSQL_URI;
-const caPath = process.env.CA_PATH;
-const pool = require("./db/connect");
+
 
 app.use(express.json());
 app.use(cors());
 
 
+const postsRouter = require("./routes/posts");
 
 
-app.get ("/test", (req, res) => {
+app.use("/posts", postsRouter);
+
+app.get ("/", (req, res) => {
     res.send("Hello World, IT WORKS");
 });
 
@@ -25,7 +26,7 @@ const port = process.env.PORT || 9000;
 
 const start = async () => {
    try {    
-    await pool(dbUrl, caPath);
+    
     app.listen(port, () => console.log(`Server is listening on port ${port} and Database is connected`));
    } catch (error) {
     console.log(error);
