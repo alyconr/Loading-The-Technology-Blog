c706764a6e8ed5e5727b257070e7d0ed9af835a2
alyconr <alyconr@hotmail.com>
2023-11-02 16:29:20 -0500

    :hammer: Configure pool connection with ssl certificate

---
+++ Backend/controllers/posts.js
@@ -0,0 +1,28 @@
+const { StatusCodes } = require('http-status-codes');
+
+
+const dbUrl = process.env.MYSQL_URI;
+const caPath = process.env.CA_PATH;
+const pool = require('../db/connect')(dbUrl, caPath);
+
+
+
+const getAllPosts = async (req, res) => {
+    const sql = 'SELECT * FROM posts';
+
+    pool.query(sql, (queryError, results) => {
+        if(queryError) {
+            console.error('Database query error:', queryError);
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Database query error' });
+
+        } else {
+            res.status(StatusCodes.OK).json({ posts: results });
+        }
+
+    })
+
+}
+
+module.exports = {
+    getAllPosts
+}
