e7c874fac1731efdd22f76706e95e34d7f40db7c
alyconr <alyconr@hotmail.com>
2023-11-14 10:36:39 -0500

    :zap: Add controllers to the main

--- Backend/app.js
+++ Backend/app.js
@@ -1,40 +1,35 @@
 require("dotenv").config();
-require("express-async-errors");
 const express = require("express");
 const app = express();
-
 const cors = require("cors");
-
-
 
 app.use(express.json());
 app.use(cors());
 
-
+const authRouter = require("./routes/auth");
 const postsRouter = require("./routes/posts");
 
 
-app.use("/posts", postsRouter);
 
-app.get ("/", (req, res) => {
-    res.send("Hello World, IT WORKS");
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/posts", postsRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello World, IT WORKS");
 });
-
 
 const port = process.env.PORT || 9000;
 
-
 const start = async () => {
-   try {    
-    
-    app.listen(port, () => console.log(`Server is listening on port ${port} and Database is connected`));
-   } catch (error) {
+  try {
+    app.listen(port, () =>
+      console.log(
+        `Server is listening on port ${port} and Database is connected`
+      )
+    );
+  } catch (error) {
     console.log(error);
-   }
-}
+  }
+};
 
 start();
-
-
-
-
