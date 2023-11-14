252fef26f11182c71077d9aa3de2c33c146dc7a6
alyconr <alyconr@hotmail.com>
2023-11-14 12:00:00 -0500

    :fire: Delete unnecessary files

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Write from "./pages/Write";
+import Singlepost from "./pages/Singlepost";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const Layout = () => {
+  
   return (
+    
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
   )
+   
 }
+
+
+const Router = createBrowserRouter([
+
+  {
+    path: "/",
+    element:  <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/write",
+        element: <Write />
+      },
+      {
+        path: "/singlepost/:id",
+        element: <Singlepost />
+      }
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  
+
+])
+
+function App() {
+
+  return (
+    <div>
+      <RouterProvider router={Router} />
+    </div>
+  )
+}
+
+
 
 export default App
