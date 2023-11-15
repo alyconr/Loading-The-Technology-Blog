ef8fc7a6dba9de33d8512e4e0c5f2da120b9f92a
alyconr <alyconr@hotmail.com>
2023-11-15 17:24:23 -0500

    :tada: Upload footer and navbar

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,9 +1,52 @@
+import styled from "styled-components";
+
 const Footer = () => {
     return ( 
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <Footers>
+            <img src="../../logo.png" alt="logo" />
+            <span>Made with ❤️ by <a href="https://github.com/alyconr"> Alyconr</a> </span>
+        </Footers>
+
      );
 }
  
 export default Footer;
+
+const Footers = styled.footer`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 70px;
+    background-color: #333;
+    color: #fff;
+    font-size: 14px;
+    font-weight: bold;
+    text-align: center;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    margin-top: 50px;
+
+    span {
+        margin-right: 10px;
+        margin-left: 10px;
+        color: #fff;
+        text-decoration: none;
+        &:hover {
+            text-decoration: underline;
+            cursor: pointer;
+        }
+    }
+    a {
+        color: #fff;
+        text-decoration: none;
+        &:hover {
+            text-decoration: underline;
+        }
+
+    }
+    img {
+        width: 250px;
+        height:250px;
+        margin-right: 10px;
+    }
+`
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,12 +1,107 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-    return ( 
-        <div>
-            <h1>Navbar</h1>
-        </div>
-      );
-}
- 
-export default Navbar; 
-<div>
-    
-</div>
+  return (
+    <NavBar>
+      <div className="container">
+        <Logo>
+          <img src="logo.png" alt="logo" />
+        </Logo>
+        <Menu>
+          <MenuItem to="/">Home</MenuItem>
+          <MenuItem to="/?category=Javascript">Javascript</MenuItem>
+          <MenuItem to="/?category=CSS">CSS</MenuItem>
+          <MenuItem to="/?category=React">React</MenuItem>
+          <MenuItem to="/?category=Node">Node</MenuItem>
+          <MenuItem to="/?category=Python">Python</MenuItem>
+          <MenuItem to="/?category=Docker">Docker</MenuItem>
+          <Span>John</Span>
+          <Span>Logout</Span>
+          <Span>
+            <Write to="/write">Write</Write>
+          </Span>
+        </Menu>
+      </div>
+    </NavBar>
+  );
+};
+
+export default Navbar;
+
+const NavBar = styled.div`
+  width: 100%;
+  height: 70px;
+  background-color: #342e2e;
+  position: sticky;
+  top: 0;
+  z-index: 999;
+  .container {
+    padding: 0px 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 100%;
+  }
+`;
+const Logo = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+
+  img {
+    height: 200px;
+    width: 250px;
+    cursor: pointer;
+  }
+`;
+
+const Menu = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: space-between;
+  gap: 12px;
+`;
+
+const MenuItem = styled(Link)`
+  font-size: 20px;
+  cursor: pointer;
+  text-decoration: none;
+  color: white;
+  font-weight: 500;
+
+  &:hover {
+    transform: scale(1.1);
+    transition: all 0.3s ease;
+    text-decoration: underline;
+    cursor: pointer;
+    color: #12c4c1;
+  }
+`;
+
+const Span = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  text-decoration: none;
+  color: white;
+  font-weight: 500;
+  margin-left: 20px;
+  background-color: #1dbf73;
+  padding: 10px;
+  border-radius: 5px;
+  transition: all 0.3s ease;
+
+  &:hover {
+    transform: scale(1.1);
+    transition: all 0.3s ease;
+    cursor: pointer;
+  }
+`;
+
+const Write = styled(Link)`
+  font-size: 14px;
+  cursor: pointer;
+  text-decoration: none;
+  color: white;
+`;
