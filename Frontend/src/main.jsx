a3118b0e0e202d6579986d949a0946178d2197d5
alyconr <alyconr@hotmail.com>
2023-11-22 20:59:58 -0500

    :zap: Configure auth context

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
+import {  AuthContextProvider } from "./context/authContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>
 );
