81a08c27224454401ad2cb5fb4320c62ab119225
alyconr <alyconr@hotmail.com>
2023-11-22 21:11:21 -0500

    :hammer: Add upload feature with multer and  cookies parse middleware

--- Backend/app.js
+++ Backend/app.js
@@ -1,15 +1,38 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const multer = require("multer");
+
+const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cookieParser());
+
+// multer
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../Frontend/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
+
+app.post("/api/v1/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 
 const authRouter = require("./routes/auth");
 const postsRouter = require("./routes/posts");
-
-
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/posts", postsRouter);
