dd96e46ad3f2f4453734cf12751ccf4a4f00a105
alyconr <alyconr@hotmail.com>
2023-11-22 21:13:55 -0500

    :globe_with_meridians: Setting router routes

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 
-const { register, login } = require("../controllers/auth");
+const { register, login, logout } = require("../controllers/auth");
 
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", logout);
 
 
 module.exports = router;
--- Backend/routes/posts.js
+++ Backend/routes/posts.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllPosts } = require("../controllers/posts");
+const { getAllPosts, getSinglePost, createPost, deletePost, updatePost } = require("../controllers/posts");
 
-router.route("/").get(getAllPosts);
-
+router.route("/").get(getAllPosts).post(createPost);
+router.route("/:id").get(getSinglePost).delete(deletePost).put(updatePost);
 module.exports = router;
