e7ab0ce4e50c739bcec0a4e1948624838960e87f
alyconr <alyconr@hotmail.com>
2023-11-24 17:49:06 -0500

    :hammer: Adding usefetch method

---
+++ Frontend/src/utils/useFetch.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      axios
+        .get(url)
+        .then((response) => {
+          if (response.status !== 200) {
+            throw Error("could not fetch the data");
+          }
+          setData(response.data.posts);
+          setLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setLoading(false);
+          setData(null);
+        });
+    }, 500);
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
