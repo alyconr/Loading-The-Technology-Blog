80901409a175732fabec47e8cb465145dc0644cf
alyconr <alyconr@hotmail.com>
2023-12-04 09:22:25 -0500

    :heavy_plus_sign: Adjust route posts

--- Backend/routes/posts.js
+++ Backend/routes/posts.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+
+
+
 const { getAllPosts, getSinglePost, createPost, deletePost, updatePost } = require("../controllers/posts");
 
 router.route("/").get(getAllPosts).post(createPost);
 router.route("/:id").get(getSinglePost).delete(deletePost).put(updatePost);
+
 module.exports = router;
