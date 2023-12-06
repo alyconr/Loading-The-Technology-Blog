01e9c44009147fa28499853eef1f9672462e46c8
alyconr <alyconr@hotmail.com>
2023-12-06 12:41:37 -0500

    :sparkles: Ad svg spark  design

---
+++ Frontend/src/components/SparkSvg.jsx
@@ -0,0 +1,19 @@
+// SparkSvg.js
+import React from "react";
+
+const SparkSvg = () => (
+    <svg width="80" height="80" viewBox="0 0 46 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M23.5 12L21.3349 3L25.6651 3L23.5 12Z" fill="#1c1e85"/>
+    <path d="M12.2405 19.6651L3.36372 17.0401L5.52878 13.2901L12.2405 19.6651Z" fill="#1c1e85"/>
+    <path d="M34.25 19.6651L40.9617 13.2901L43.1268 17.0401L34.25 19.6651Z" fill="#1c1e85"/>
+    <path d="M30.602 32.9192L38.8987 37.0244L36.1239 40.3486L30.602 32.9192Z" fill="#1c1e85"/>
+    <path d="M16.1178 32.9193L10.5956 40.3485L7.82097 37.0242L16.1178 32.9193Z" fill="#1c1e85"/>
+    <rect x="34" y="14" width="2" height="2" fill="#89948C"/>
+    <rect x="18" y="9" width="2" height="2" fill="#89948C"/>
+    <rect x="8" y="22" width="2" height="2" fill="#89948C"/>
+    <rect x="18" y="35" width="2" height="2" fill="#89948C"/>
+    <rect x="34" y="29" width="2" height="2" fill="#89948C"/>
+    </svg>
+);
+
+export default SparkSvg;
