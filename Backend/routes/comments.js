28034a9898d6954e2652df7801faced40de0d63a
alyconr <alyconr@hotmail.com>
2024-01-02 10:30:19 -0500

    :sparkles: Implement   comments controller

--- Backend/routes/comments.js
+++ Backend/routes/comments.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 
-const { createComment, getComments  } = require("../controllers/comments");
+const { createComment, getComments, deleteComment  } = require("../controllers/comments");
 
-router.route("/:id").post(createComment);
-router.route("/:id").get(getComments);
+router.route("/:id").post(createComment).get(getComments);
+router.route("/postId/:id").delete(deleteComment);
+
+
 
 
 module.exports = router;
