0201e3e747ac0da4b713dcb3dc60b61f87d18e01
alyconr <alyconr@hotmail.com>
2024-01-08 17:56:57 -0500

    :sparkles: New feature added to route  comments on comments feature

--- Backend/routes/commentsOnComments.js
+++ Backend/routes/commentsOnComments.js
@@ -5,8 +5,14 @@ const router = express.Router();
 const {
   createCommentsOnComment,
   getCommentsOnComments,
+  deleteCommentsOnComment,
+  updateCommentOnComment,
 } = require("../controllers/commentsOnComment");
 
 router.route("/:id").post(createCommentsOnComment).get(getCommentsOnComments);
+router
+  .route("/:id")
+  .delete(deleteCommentsOnComment)
+  .put(updateCommentOnComment);
 
 module.exports = router;
