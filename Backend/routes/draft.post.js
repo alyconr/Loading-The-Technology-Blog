c5ddb385d20ecd5005ad9fc58c971c4714ec1226
alyconr <alyconr@hotmail.com>
2024-01-15 16:10:21 -0500

    :construction: Add controllers routes

---
+++ Backend/routes/draft.post.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllDraftPosts,
+  createDraftPost,
+} = require("../controllers/draft.post");
+
+router.route("/").get(getAllDraftPosts).post(createDraftPost);
+
+module.exports = router;
