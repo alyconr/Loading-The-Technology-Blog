38f9c811182ddb3d5456829720d832a35b47bbc6
alyconr <alyconr@hotmail.com>
2024-01-16 15:28:54 -0500

    :sparkles: Adding new router route to gets draftId

--- Backend/routes/draft.post.js
+++ Backend/routes/draft.post.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const {
   getAllDraftPosts,
   createDraftPost,
+  updateDraftPost,
 } = require("../controllers/draft.post");
 
-router.route("/").get(getAllDraftPosts).post(createDraftPost); 
+router.route("/").get(getAllDraftPosts).post(createDraftPost);
+router.route("/:id").put(updateDraftPost);
 
 module.exports = router;
