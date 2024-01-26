58d1ca4b9c63e268d23f079e6f837ad12c3a0cde
alyconr <alyconr@hotmail.com>
2024-01-26 17:47:48 -0500

    sparkles: Adding  endpoints routes to handle restore passwords

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login, logout } = require("../controllers/auth");
+const { register, login, logout, requestPasswordReset, resetPassword } = require("../controllers/auth");
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
+router.post("/requestPasswordReset", requestPasswordReset);
+router.post("/resetPassword", resetPassword);
 
 module.exports = router;
