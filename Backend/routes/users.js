7e6581047e5c93be81655cb3f751b5bebd786173
alyconr <alyconr@hotmail.com>
2024-02-15 16:26:56 -0500

    :art: Format code

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 const authorizedUser = require("../middleware/authorizedUser");
 
-const { getCurrentUser, updateUser, getUserPosts, getAllUsers } = require("../controllers/users");
+const { getCurrentUser, updateUser, getUserPosts, getAllUsers, deleteUser } = require("../controllers/users");
 
-router.route("/:username").get(getCurrentUser).put(authorizedUser, updateUser);;
+router.route("/:username").get(getCurrentUser).put(authorizedUser, updateUser).delete(deleteUser);
 router.route("/posts/:username").get(getUserPosts);
 router.route("/").get(getAllUsers);
 
