471e06e34ee4e3822dcb2d8875cd6a39f3bbad12
alyconr <alyconr@hotmail.com>
2024-02-27 15:48:34 -0500

    :boom:  Apply somme changes to get Total followers functions on backend controllers

--- Backend/controllers/followers.js
+++ Backend/controllers/followers.js
@@ -20,8 +20,10 @@ const createFollower = async (req, res) => {
     return res.status(StatusCodes.UNAUTHORIZED).json({ error: "Unauthorized" });
   }
 
-  if(decoded.id === req.body.following_id){
-    return res.status(StatusCodes.BAD_REQUEST).json({ error: "Can't follow yourself" });
+  if (decoded.id === req.body.following_id) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: "Can't follow yourself" });
   }
 
   const sql =
@@ -62,10 +64,10 @@ const getTotalFollowers = async (req, res) => {
 
 const getUserFollowers = async (req, res) => {
   const sql =
-  "SELECT users.id, `fullname`, users.image AS userImage, SUM(followers.followers_count) AS total_followers FROM followers " +
-  "JOIN users ON followers.follower_id = users.id " +
-  "WHERE following_id = ? " +
-  "GROUP BY users.id, `fullname` , users.image";
+    "SELECT users.id, `fullname`, users.image AS userImage, SUM(followers.followers_count) AS total_followers FROM followers " +
+    "JOIN users ON followers.follower_id = users.id " +
+    "WHERE following_id = ? " +
+    "GROUP BY users.id, `fullname` , users.image";
 
   const values = [req.params.id];
 
@@ -73,14 +75,11 @@ const getUserFollowers = async (req, res) => {
     if (queryError) {
       console.error("Database query error:", queryError);
       res.status(StatusCodes.INTERNAL_SERVER_ERROR);
-      
     } else {
       res.status(StatusCodes.OK).json(results);
-
     }
-  })
-}
-
+  });
+};
 
 const unfollowUser = async (req, res) => {
   const token = req.cookies.token;
@@ -106,20 +105,20 @@ const unfollowUser = async (req, res) => {
   pool.query(sql, values, (queryError, results) => {
     if (queryError) {
       console.error("Database query error:", queryError);
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR). 
-      json({ error: "Database query error" });
-
+      res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ error: "Database query error" });
     } else {
       res
         .status(StatusCodes.OK)
         .json({ message: "Follower deleted successfully" });
     }
-  })
-}
+  });
+};
 
 module.exports = {
   createFollower,
   getTotalFollowers,
   getUserFollowers,
-  unfollowUser
+  unfollowUser,
 };
