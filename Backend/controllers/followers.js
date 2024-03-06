703580ace0039aa9b2eca885c008be4c01b5334c
alyconr <alyconr@hotmail.com>
2024-03-05 20:26:13 -0500

    :boom: Introduce a feature to the followers controller to  sum all the followers

--- Backend/controllers/followers.js
+++ Backend/controllers/followers.js
@@ -43,24 +43,44 @@ const createFollower = async (req, res) => {
   });
 };
 
-const getTotalFollowers = async (req, res) => {
+const getTotalFollowers = (req, res) => {
   const sql =
-    "SELECT users.id, `username`, users.image AS userImage,   SUM (followers.followers_count) AS total_followers FROM followers " +
-    "JOIN users ON followers.following_id = users.id " +
-    "WHERE following_id = ? " +
-    "GROUP BY users.id, `username` , users.image ";
+    "SELECT users.id, followers.following_id, users.username, users.image AS userImage, SUM(followers.followers_count) AS total_followers " +
+    "FROM followers " +
+    "JOIN users ON followers.follower_id = users.id " +
+    "WHERE followers.following_id = ? " +
+    "GROUP BY users.id, followers.following_id, users.username, users.image";
 
   const values = [req.params.id];
 
   pool.query(sql, values, (queryError, results) => {
     if (queryError) {
       console.error("Database query error:", queryError);
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Database query error" });
     } else {
-      res.status(StatusCodes.OK).json(results);
+      // Calculate the sum of total_followers
+      const totalFollowersSum = results.reduce(
+        (sum, result) => 
+          sum + parseInt(result.total_followers, 10), 0
+        
+        );
+
+       // Add the totalFollowersSum as a number to each result
+      const resultsWithSum = results.map(result => ({
+        ...result,      
+      }));
+
+      // Append the total sum as a number to the end of the array
+      resultsWithSum.push({
+        total_followers_sum: totalFollowersSum
+      });
+
+      res.status(StatusCodes.OK).json(resultsWithSum);
     }
   });
 };
+
+
 
 const getUserFollowers = async (req, res) => {
   const sql =
