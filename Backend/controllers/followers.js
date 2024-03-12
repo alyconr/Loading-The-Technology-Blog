32172c5d01d9b51cfe57191425bcb696ddbc945c
alyconr <alyconr@hotmail.com>
2024-03-12 11:54:54 -0500

    :fire: Remove code total followers from  get user followers function

--- Backend/controllers/followers.js
+++ Backend/controllers/followers.js
@@ -45,42 +45,25 @@ const createFollower = async (req, res) => {
 
 const getTotalFollowers = (req, res) => {
   const sql =
-    "SELECT users.id, followers.following_id, users.username, users.image AS userImage, SUM(followers.followers_count) AS total_followers " +
+    "SELECT users.id, followers.following_id, users.username, users.fullname, users.image AS userImage, SUM(followers.followers_count) AS total_followers " +
     "FROM followers " +
     "JOIN users ON followers.follower_id = users.id " +
     "WHERE followers.following_id = ? " +
-    "GROUP BY users.id, followers.following_id, users.username, users.image";
+    "GROUP BY users.id, followers.following_id, users.username, users.fullname, users.image";
 
   const values = [req.params.id];
 
   pool.query(sql, values, (queryError, results) => {
     if (queryError) {
       console.error("Database query error:", queryError);
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Database query error" });
+      res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ error: "Database query error" });
     } else {
-      // Calculate the sum of total_followers
-      const totalFollowersSum = results.reduce(
-        (sum, result) => 
-          sum + parseInt(result.total_followers, 10), 0
-        
-        );
-
-       // Add the totalFollowersSum as a number to each result
-      const resultsWithSum = results.map(result => ({
-        ...result,      
-      }));
-
-      // Append the total sum as a number to the end of the array
-      resultsWithSum.push({
-        total_followers_sum: totalFollowersSum
-      });
-
-      res.status(StatusCodes.OK).json(resultsWithSum);
+      res.status(StatusCodes.OK).json(results);
     }
   });
 };
-
-
 
 const getUserFollowers = async (req, res) => {
   const sql =
