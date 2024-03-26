9ffdb551f1d1a5dbd56c4f50ac0476c1d39bb679
alyconr <alyconr@hotmail.com>
2024-03-26 16:48:10 -0500

    :construction: Adding routes to handle a single bookmark

--- Backend/routes/bookmarks.js
+++ Backend/routes/bookmarks.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { createBookmark, getAllBookmarks, deleteBookmark } = require("../controllers/bookmarks");
+const { createBookmark, getAllBookmarks, deleteBookmark, getSingleBookmark } = require("../controllers/bookmarks");
 
 router.route("/").post(createBookmark).get(getAllBookmarks);
-router.route("/:id").delete(deleteBookmark);
+router.route("/:id").delete(deleteBookmark).get(getSingleBookmark);
 
 module.exports = router;
