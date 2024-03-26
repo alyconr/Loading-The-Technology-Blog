6df5bb3d30b7b2ce42413c7b1cb4b3be37cf6026
alyconr <alyconr@hotmail.com>
2024-03-26 15:54:12 -0500

    :heavy_plus_sign: Ading bookmark routes

--- Backend/routes/bookmarks.js
+++ Backend/routes/bookmarks.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { createBookmark, getAllBookmarks } = require("../controllers/bookmarks");
+const { createBookmark, getAllBookmarks, deleteBookmark } = require("../controllers/bookmarks");
 
 router.route("/").post(createBookmark).get(getAllBookmarks);
+router.route("/:id").delete(deleteBookmark);
 
 module.exports = router;
