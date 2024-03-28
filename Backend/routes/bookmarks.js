0b3f76a02c7a42c640b5154e9801e879aa4da035
alyconr <alyconr@hotmail.com>
2024-03-27 21:41:58 -0500

    :boom: Adjust routes to retrive  bookmarks data

--- Backend/routes/bookmarks.js
+++ Backend/routes/bookmarks.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { createBookmark, getAllBookmarks, deleteBookmark, getSingleBookmark } = require("../controllers/bookmarks");
+const { createBookmark, getAllBookmarks, deleteBookmark } = require("../controllers/bookmarks");
 
-router.route("/").post(createBookmark).get(getAllBookmarks);
-router.route("/:id").delete(deleteBookmark).get(getSingleBookmark);
+router.route("/").post(createBookmark)
+router.route("/:id").delete(deleteBookmark).get(getAllBookmarks);
 
 module.exports = router;
